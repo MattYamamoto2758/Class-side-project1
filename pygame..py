21410aca51033de46f3f17fe5a765f35345dcaba
MattYamamoto2758 <36433913+MattYamamoto2758@users.noreply.github.com>
2018-03-02 14:29:51 -0800

    Add files via upload

---
+++ pygame..py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_mode((640,480))
+
+def game():
+
+    width, hight = 600, 400
+    screen=pygame.display.set_mode((width,hight))
+    pygamy.display.set_caption('My First game')
